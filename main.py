52528dbae0565fa1fe90cacdb974c2ed62631f9b
Lutz Wolf <74728972+lutzwolfer@users.noreply.github.com>
2026-04-21 10:44:08 +0200

    added args to save and show the images

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import mss
 import numpy as np
 import cv2
@@ -23,9 +24,10 @@ def capture_all_monitors():
         combined_screenshot = cv2.hconcat(screenshots)
         return combined_screenshot
 
-def main():
-    debug_dir = "image_output"
-    os.makedirs(debug_dir, exist_ok=True)
+def main(image_show: bool = False, image_save: bool = False) -> None:
+    if image_save:
+        debug_dir = "image_output"
+        os.makedirs(debug_dir, exist_ok=True)
 
     screenshot = capture_all_monitors()
 
@@ -36,20 +38,26 @@ def main():
         marked_img = screenshot.copy()
         cv2.drawContours(marked_img, [cv2.approxPolyDP(np.array([[x, y], [x + w, y], [x + w, y + h], [x, y + h]]), 0, True)], -1, (0, 255, 0), 3)
         print(f"Board found: start point ({x}, {y}), width {w}, height {h}")
-        save_image(marked_img, "01_chessboard_marked.png", debug_dir)
         chessboard_img = marked_img[y:y+h, x:x+w]
-        save_image(chessboard_img, "02_chessboard.png", debug_dir)
-        width = 1280
-        scale_factor = width / marked_img.shape[1]
-        new_height = int(marked_img.shape[0] * scale_factor)
-        resized_image = cv2.resize(marked_img, (width, new_height))
-        resized_image2 = cv2.resize(chessboard_img, (new_height, new_height))
-        images_to_show = cv2.hconcat([resized_image, resized_image2])
-        cv2.imshow('detected chessboard', images_to_show)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if image_save:
+            save_image(marked_img, "01_chessboard_marked.png", debug_dir)
+            save_image(chessboard_img, "02_chessboard.png", debug_dir)
+        if image_show:
+            width = 1280
+            scale_factor = width / marked_img.shape[1]
+            new_height = int(marked_img.shape[0] * scale_factor)
+            resized_image = cv2.resize(marked_img, (width, new_height))
+            resized_image2 = cv2.resize(chessboard_img, (new_height, new_height))
+            images_to_show = cv2.hconcat([resized_image, resized_image2])
+            cv2.imshow('detected chessboard', images_to_show)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
     else:
         print("No Board found")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_show", help="Show Images.", action="store_true")
+    parser.add_argument("--image_save", help="Save Images to File.", action="store_true")
+    args = parser.parse_args()
+    main(image_show=args.image_show, image_save=args.image_save)
